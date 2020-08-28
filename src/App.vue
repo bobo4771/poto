c7533508b60f4b8460cce84a8e82a60cb913e7ba
bobo4771 <68528767+bobo4771@users.noreply.github.com>
2020-08-28 23:54:28 +0700

    commid all

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+ 
+    <HelloWorld bobo="ประเทศไทย" />
+    <hr>
+    <div class="row">
+      <div class="col-6">
+   <img src="./assets/prao1.png" width="500" height="300" /> 
+     
+     
+        <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+  <img src="./assets/paro.png" width="350" height="300" />
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-6">
+        <CsButton @cc="sum" />
+      </div>
+      <div class="col-6">
+        <CsButton @cc="sem" />
+      </div>
+    </div>
+    
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import CsButton from "./components/CsButton.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CsButton,
+    
+  },
+  data : function(){
+    return {
+    total1: 0,
+    total2: 0,
+   
+   } 
+  },
+  methods: {
+  sum(counterValue){
+    this.total1 = counterValue
+  },
+  
+  sem(counterValue){
+    this.total2 = counterValue
+ 
+  
+},
+},
+
+};
+
 </script>
 
 <style>
@@ -22,7 +63,13 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0b44ff;
   margin-top: 60px;
+  
+}
+body {
+background-image: url("./assets/bg.jpg");
+  background-size: 100%;
 }
 </style>
+
---
+++ src/components/CsButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button v-on:click="upFunction" class="btn btn-#42b983"> <img src="../assets/like.gif" width="100" height="100" > {{lable}}</button>
+    <button v-on:click="downFunction" class="btn btn-#42b983 ml-3"><img src="../assets/dd.gif" width="70" height="70" > {{lable}}</button>
+    <button v-on:click="LoveFunction" class="btn btn-#42b983 ml-3"><img src="../assets/loev.gif" width="100" height="100" > {{lable}}</button>
+    <button v-on:click="ResetFunction" class="btn btn-#42b983 ml-3"><img src="../assets/f.png" width="100" height="100" > {{lable}}</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    upFunction: function () {
+      this.counter += 1;
+      this.$emit("cc", this.counter);
+    },
+
+    downFunction: function () {
+      this.counter += -5;
+      this.$emit("cc", this.counter);
+    },
+
+    LoveFunction: function () {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+<style>
+</style>
